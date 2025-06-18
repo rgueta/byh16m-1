0e3368607f09f02d03c9c2e4a46620498f3ecc59
rgueta <rgueta@debian>
2025-06-18 09:42:25 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'byh16m',
+  webDir: 'www'
+};
+
+export default config;
